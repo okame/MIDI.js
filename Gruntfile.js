caa5a1cc6683adeb9346e47310a6ef837ecfdaec
okame <pikkoro888@gmail.com>
2017-10-25 15:45:55 +0900

    add base64binary to package

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,36 +12,29 @@
 */
 
 module.exports = function (grunt) {
-	grunt.initConfig({
-		concat: {
-			'build/MIDI.js': [
-//				'js/color/spaceW3.js', // optional
-				'js/midi/audioDetect.js',
-				'js/midi/gm.js',
-				'js/midi/loader.js',
-				'js/midi/player.js',
-				'js/midi/plugin.audiotag.js',
-				'js/midi/plugin.webaudio.js',
-				'js/midi/plugin.webmidi.js',
-//				'js/midi/synesthesia.js', // optional
-				'js/util/dom_request_xhr.js', // req when using XHR
-				'js/util/dom_request_script.js', // req otherwise
-//				'js/widget/loader.js', // optional
-				'inc/polyfill/Base64.js',
-				'inc/polyfill/base64binary.js'
-			]
-		},
-		uglify: {
-			'build/MIDI.min.js': [
-				'build/MIDI.js'
-			]
-		}
-	});
+  grunt.initConfig({
+    concat: {
+      'build/MIDI.js': [
+        'js/midi/audioDetect.js',
+        'js/midi/gm.js',
+        'js/midi/loader.js',
+        'js/midi/player.js',
+        'js/midi/plugin.audiotag.js',
+        'js/midi/plugin.webaudio.js',
+        'js/midi/plugin.webmidi.js',
+        'js/util/dom_request_xhr.js', // req when using XHR
+        'js/util/dom_request_script.js', // req otherwise
+        'inc/shim/base64binary.js'
+      ]
+    },
+    uglify: {
+      'build/MIDI.min.js': [
+        'build/MIDI.js'
+      ]
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	///
-	grunt.registerTask('default', ['concat', 'uglify']);
-	///
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat', 'uglify']);
 };
